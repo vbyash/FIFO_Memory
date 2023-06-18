ef6ad65b54d08901d159548ec668623a55ca75c3
vbyash <129272123+vbyash@users.noreply.github.com>
2023-06-18 20:02:20 +0530

    Update test.v

--- test.v
+++ test.v
@@ -1,20 +1,22 @@
 module FIFO_test;
- wire [31:0] out;
+ wire [15:0] out;
  wire ful, emp;
  reg clocks, rst, clr, wn, rn;
- reg [31:0] in;  
+ reg [15:0] in;  
   
  FIFO mem(.clock(clocks), .reset(rst), .clear(clr), .write(wn), .read(rn), .data_in(in), .full(ful), .empty(emp), .data_out(out));
   
  initial 
   begin
+   rst=0;
+   clr=0;
    clocks=0;
   forever #5 clocks=~clocks;
   end
  
  initial 
   begin
-   $monitor($time, , ,"c=%b",clk,,"reset=%b",rst,,"data_in=%b",in,,"clear=%b",clr,,"read=%b",rn,,"write=%b",wn,,"full=%b",ful,,"empty=%b",emp,,"data_out=%b",out);
+   $monitor($time, , ,"c=%b",clocks,,"reset=%b",rst,,"data_in=%b",in,,"clear=%b",clr,,"read=%b",rn,,"write=%b",wn,,"full=%b",ful,,"empty=%b",emp,,"data_out=%b",out);
    #300 $display("Finished");
    $finish;
   end
@@ -27,27 +29,29 @@ module FIFO_test;
 
   initial
   begin
-   in = 31'd0;
+   in = 15'd0;
    rst = 1; 
   #10 rst = 0;
   // First write some data into the queue
    #10 wn = 1; rn = 0;
-   in = 31'd100;
-   #10 in = 31'd150;
-   #10 in = 31'd200;
+   in = 15'd100;
+   #10 in = 15'd150;
+   #10 in = 15'd200;
    #10 wn = 0; rn = 1;
-   #20 clr = 1;
-   #10 wn = 1; rn = 0;
-   in = 31'd100;
-   #10 in = 31'd150;
-   #10 in = 31'd200;
-   #10 in = 31'd40;
-   #10 in = 31'd70;
-   #10 in = 31'd65;
-   #10 in = 31'd15;
-   #10 in = 31'd230;
-   
+   #40 rn = 0;
+   #10 clr = 1; 
+ 
+   #10 clr = 0; wn = 1; in = 15'd100;   
+   #10 in = 15'd150;
+   #10 in = 15'd200;
+   #10 in = 15'd40; rn = 1;
+   #10 in = 15'd70;
+   #10 in = 15'd65;
+   #10 in = 15'd15;
+   #10 in = 15'd230;
+   #10 in = 15'd150;
+   #10 in = 15'd200;
    // Now start reading and checking the values
-   #20 wn = 0; rn = 1;
+   #30	wn = 0;
   end
 endmodule

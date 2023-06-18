78a948cbd1fe98e0b33550ed3355fb01188d5047
vbyash <129272123+vbyash@users.noreply.github.com>
2023-06-18 12:03:58 +0530

    Update test.v

--- test.v
+++ test.v
@@ -6,17 +6,19 @@ module FIFO_test;
   
  FIFO mem(.data_out(out), .full(ful), .empty(emp), .clk(clock), .reset(rst), .wr(wn), .rd(rn), .data_in(in));
   
-  //enabling the wave dump
  initial 
   begin
    clk=0;
   forever #5 clk=~clk;
   end
+ 
  initial 
   begin
    $monitor($time, , ,"c=%b",clk,,"y=%b",out,,"r=%b",rst,,"d=%b",in,,"read=%b",rn,,"write=%b",wn,,"full=%b",ful,,"empty=%b",emp);
    #200 $display("Finished");
    $finish;
+  end
+ 
   initial 
    begin 
    $dumpfile("dump.vcd"); 
@@ -25,52 +27,20 @@ module FIFO_test;
 
   initial
   begin
-   clock = 0; 
    in = 8'd0;
-   reset = 1; clock = 1; #5 ; clock = 0; #5;
-   reset = 0;
-  
+   reset = 1; 
+  #10 reset = 0;
   // First write some data into the queue
-   wn = 1; rn = 0;
-   in = 8'd100;
-   clock = 1; #5 ; 
-   clock = 0; #5;
-   in = 8'd150;
-   clock = 1; #5 ;
-   clock = 0; #5;
-   in = 8'd200;
-   clock = 1; #5 ;
-   clock = 0; #5;
-   in = 8'd40;
-   clock = 1; #5 ; 
-   clock = 0; #5;
-   in = 8'd70;
-   clock = 1; #5 ;
-   clock = 0; #5;
-   in = 8'd65;
-   clock = 1; #5 ;
-   clock = 0; #5;
-   in = 8'd15;
-   clock = 1; #5;
-   clock = 0; #5;
-    
-   // Now start reading and checking the values
-   wn = 0; rn = 1;
-   clock = 1; #5;
-   clock = 0; #5;
-   clock = 1; #5;
-   clock = 0; #5;
-   clock = 1; #5;
-   clock = 0; #5;
-   clock = 1; #5;
-   clock = 0; #5;
-   clock = 1; #5;
-   clock = 0; #5;
-   clock = 1; #5; 
-   clock = 0; #5;
-   clock = 1; #5; 
-   clock = 0; #5;
-   clock = 1; #5; 
-   clock = 0; #5;
+   #10 wn = 1; rn = 0;
+   #10 in = 8'd100;
+   #10 in = 8'd150;
+   #10 in = 8'd200;
+   #10 in = 8'd40;
+   #10 in = 8'd70;
+   #10 in = 8'd65;
+   #10 in = 8'd15;
    
+   // Now start reading and checking the values
+   #20 wn = 0; rn = 1;
+  end
 endmodule

d4f128a59413595e17627bd9df0ff8bd4ca715b6
vbyash <129272123+vbyash@users.noreply.github.com>
2023-06-18 12:00:11 +0530

    Update test.v

--- test.v
+++ test.v
@@ -1,134 +1,76 @@
- `timescale     10 ps/ 10 ps  
- `define          DELAY 10  
+module FIFO_test;
+ wire [7:0] out;
+ wire ful, emp;
+ reg clock, rst, wn, rn;
+ reg [7:0] in;  
+  
+ FIFO mem(.data_out(out), .full(ful), .empty(emp), .clk(clock), .reset(rst), .wr(wn), .rd(rn), .data_in(in));
+  
+  //enabling the wave dump
+ initial 
+  begin
+   clk=0;
+  forever #5 clk=~clk;
+  end
+ initial 
+  begin
+   $monitor($time, , ,"c=%b",clk,,"y=%b",out,,"r=%b",rst,,"d=%b",in,,"read=%b",rn,,"write=%b",wn,,"full=%b",ful,,"empty=%b",emp);
+   #200 $display("Finished");
+   $finish;
+  initial 
+   begin 
+   $dumpfile("dump.vcd"); 
+   $dumpvars;
+   end
 
- module     tb_fifo_32;  
-
- parameter     ENDTIME      = 40000;  
- // 5. DUT Input regs  
- reg     clk;  
- reg     rst_n;  
- reg     wr;  
- reg     rd;  
- reg     [7:0] data_in;  
- // 6. DUT Output wires  
- wire     [7:0] data_out;  
- wire     fifo_empty;  
- wire     fifo_full;  
- wire     fifo_threshold;  
- wire     fifo_overflow;  
- wire     fifo_underflow;  
- integer i;  
- // 7. DUT Instantiation
- fifo_mem tb (/*AUTOARG*/  
-   // Outputs  
-   data_out, fifo_full, fifo_empty, fifo_threshold, fifo_overflow,   
-   fifo_underflow,   
-   // Inputs  
-   clk, rst_n, wr, rd, data_in  
-   );  
- // 8. Initial Conditions  
- initial  
-      begin  
-           clk     = 1'b0;  
-           rst_n     = 1'b0;  
-           wr     = 1'b0;  
-           rd     = 1'b0;  
-           data_in     = 8'd0;  
-      end  
- // 9. Generating Test Vectors  
- initial  
-      begin  
-           main;  
-      end  
- task main;  
-      fork  
-           clock_generator;  
-           reset_generator;  
-           operation_process;  
-           debug_fifo;  
-           endsimulation;  
-      join  
- endtask  
- task clock_generator;  
-      begin  
-           forever #`DELAY clk = !clk;  
-      end  
- endtask  
- task reset_generator;  
-      begin  
-           #(`DELAY*2)  
-           rst_n = 1'b1;  
-           # 7.9  
-           rst_n = 1'b0;  
-           # 7.09  
-           rst_n = 1'b1;  
-      end  
- endtask  
- task operation_process;  
-      begin  
-           for (i = 0; i < 17; i = i + 1) begin: WRE  
-                #(`DELAY*5)  
-                wr = 1'b1;  
-                data_in = data_in + 8'd1;  
-                #(`DELAY*2)  
-                wr = 1'b0;  
-           end  
-           #(`DELAY)  
-           for (i = 0; i < 17; i = i + 1) begin: RDE  
-                #(`DELAY*2)  
-                rd = 1'b1;  
-                #(`DELAY*2)  
-                rd = 1'b0;  
-           end  
-      end  
- endtask  
- // 10. Debug fifo  
- task debug_fifo;  
-      begin
-           $display("------------------   -----------------------");  
-           $display("----------- SIMULATION RESULT ----------------");  
-           $display("--------------       -------------------");  
-           $monitor("TIME = %d, wr = %b, rd = %b, data_in = %h",$time, wr, rd, data_in);  
-      end  
- endtask  
- // 11. Self-Checking  
- reg [5:0] waddr, raddr;  
- reg [7:0] mem[64:0];  
- always @ (posedge clk) begin  
-      if (~rst_n) begin  
-           waddr     <= 6'd0;  
-      end  
-      else if (wr) begin  
-           mem[waddr] <= data_in;  
-           waddr <= waddr + 1;  
-      end  
-      $display("TIME = %d, data_out = %d, mem = %d",$time, data_out,mem[raddr]);  
-      if (~rst_n) raddr     <= 6'd0;  
-      else if (rd & (~fifo_empty)) raddr <= raddr + 1;  
-      if (rd & (~fifo_empty)) begin  
-           if (mem[raddr]  
-            == data_out) begin  
-                $display("=== PASS ===== PASS ==== PASS ==== PASS ===");  
-                if (raddr == 16) $finish;  
-           end  
-           else begin  
-                $display ("=== FAIL ==== FAIL ==== FAIL ==== FAIL ===");  
-                $display("-------------- THE SIMUALTION FINISHED ------------");  
-                $finish;  
-           end  
-      end  
- end  
- //12. Determines the simulation limit  
- task endsimulation;  
-      begin  
-           #ENDTIME  
-           $display("-------------- THE SIMUALTION FINISHED ------------");  
-           $finish;  
-      end  
- endtask  
-initial
-begin
-$dumpfile("dump.vcd");
-$dumpvars(1);
-end
- endmodule  
+  initial
+  begin
+   clock = 0; 
+   in = 8'd0;
+   reset = 1; clock = 1; #5 ; clock = 0; #5;
+   reset = 0;
+  
+  // First write some data into the queue
+   wn = 1; rn = 0;
+   in = 8'd100;
+   clock = 1; #5 ; 
+   clock = 0; #5;
+   in = 8'd150;
+   clock = 1; #5 ;
+   clock = 0; #5;
+   in = 8'd200;
+   clock = 1; #5 ;
+   clock = 0; #5;
+   in = 8'd40;
+   clock = 1; #5 ; 
+   clock = 0; #5;
+   in = 8'd70;
+   clock = 1; #5 ;
+   clock = 0; #5;
+   in = 8'd65;
+   clock = 1; #5 ;
+   clock = 0; #5;
+   in = 8'd15;
+   clock = 1; #5;
+   clock = 0; #5;
+    
+   // Now start reading and checking the values
+   wn = 0; rn = 1;
+   clock = 1; #5;
+   clock = 0; #5;
+   clock = 1; #5;
+   clock = 0; #5;
+   clock = 1; #5;
+   clock = 0; #5;
+   clock = 1; #5;
+   clock = 0; #5;
+   clock = 1; #5;
+   clock = 0; #5;
+   clock = 1; #5; 
+   clock = 0; #5;
+   clock = 1; #5; 
+   clock = 0; #5;
+   clock = 1; #5; 
+   clock = 0; #5;
+   
+endmodule
